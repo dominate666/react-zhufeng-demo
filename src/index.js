4a1d6cd0a193822363d887a890de9f7554da3d24
luofei <1063477160@qq.com>
2023-04-14 21:40:22 -0700

    create-react-app初始化

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+      <div>培训</div>
+  </React.StrictMode>
+);
+
