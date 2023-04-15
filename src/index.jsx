469fa146ce56f04dd232c27a94740146ccd6d251
luofei <1063477160@qq.com>
2023-04-15 09:02:08 -0700

    setstate 实现数据加1

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/index.less";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+class Count extends React.Component {
+  state = {
+    num: 0,
+  };
+  render() {
+    let { num } = this.state;
+    return (
+      <>
+        <span>{num}</span>
+        <br />
+        <button
+          onClick={() => {
+            num++;
+            this.setState({
+              num,
+            });
+          }}
+        >
+          累加
+        </button>
+      </>
+    );
+  }
+}
+root.render(<Count />);
