3a7e2562ecee8374711bd02cf6fc750cf66859ea
luofei <1063477160@qq.com>
2023-04-15 17:24:13 -0700

    稀疏数组转换为密集数组

--- src/index.js
+++ src/index.js
@@ -2,30 +2,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.less";
+import { symbol } from "prop-types";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-let text = "培训";
-let flag = true;
+let list = [
+  {
+    id: 1,
+    title: "11",
+  },
+  {
+    id: 2,
+    title: "22",
+  },
+];
 root.render(
   <>
-    <h2
-      style={{
-        color: "orange",
-        fontSize: "50px",
-      }}
-      className="box"
-    >
-      {text}
-    </h2>
-
-    <button
-      style={{
-        display: flag ? "block" : "none",
-      }}
-    >
-      按钮1
-    </button>
-    
-    {flag ? <button>按钮2</button> : null}
+    <h1>今日新闻</h1>
+    <ul>
+      {list.map((item, index) => {
+        return (
+          <li key={item.id}>
+            <p>
+              {item.id}
+              {item.title}
+            </p>
+          </li>
+        );
+      })}
+    </ul>
+    {/* new Array(5)  稀疏数组不能使用foreach方法,fill后变成密集数组 */}
+    {new Array(5).fill(null).map((_, index) => {
+      return <button key={index}>按钮{index + 1}</button>;
+    })}
   </>
 );
