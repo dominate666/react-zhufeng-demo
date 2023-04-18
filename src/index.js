298049d004c4d98dca8048256ee327cc7d35ab7d
luofei <1063477160@qq.com>
2023-04-18 07:48:16 -0700

    reflect.ownkeys

--- src/index.js
+++ src/index.js
@@ -14,15 +14,12 @@ root.render(
     <h2 className="title" style={styObj}></h2>
   </>
 );
-
-// 编译后的结果
-let result = createElement(
-  React.Fragment,
-  null,
-  createElement("h2", {
-    className: "title",
-    style: styObj,
-  })
-);
-
-console.log(result);
+Array.prototype.BB = 10;
+let arr = [1, 2];
+arr[Symbol("AA")] = 50;
+let keys = Reflect.ownKeys(arr);
+let m = Object.getOwnPropertyNames(arr); //获取对象非symbol的私有属性(无关是否可枚举)
+let n = Object.getOwnPropertySymbols(arr); //获取对象的symbol的私有属性
+m = [...m, ...n];
+console.log("m", m);
+console.log("keys", keys);
